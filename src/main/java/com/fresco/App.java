2d16f22d3f4cf38777ea1e465aea7240f57359d9
Jairo Grateron <jgrateron@jgmail.com>
2022-03-28 19:06:21 -0400

    add reporte ultimos 10 year

--- src/main/java/com/fresco/App.java
+++ src/main/java/com/fresco/App.java
@@ -15,7 +15,8 @@ import com.opencsv.CSVReaderBuilder;
 
 public class App {
 
-	public static void main(String[] args) throws IOException, URISyntaxException {
+	public static void main(String[] args) throws IOException, URISyntaxException 
+	{
 		record IndicadorYear(Integer year, Double value) {};
 		record IndicadorGeneral(String countryName, String countryCode, String indicatorName, String indicatorCode,
 				List<IndicadorYear> listaYear) {};
@@ -48,7 +49,7 @@ public class App {
 		}
 		//buscar un pais
 		var consulta = listInd.stream()
-	                          .filter( ind -> ind.countryCode.equals("USA"))
+	                          .filter( ind -> ind.countryCode.equals("VEN"))
 	                          .findFirst();
 		if (consulta.isEmpty()) {
 			System.out.println("No se encontro el registro");
@@ -56,28 +57,34 @@ public class App {
 		else {
 			System.out.println(consulta.get().countryName);
 			consulta.get().listaYear.forEach( iy -> {
-				System.out.println(String.format("\t %d %.4f", iy.year,iy.value));
+				String pib = String.format("%.4f", iy.value);
+				pib = " ".repeat(15 - pib.length()) + pib;
+				System.out.println(String.format("\t %d %s", iy.year,pib));
 			});
 		}
+		var maxName = listInd.stream()
+				         .mapToInt(i -> i.countryName.length())
+				         .max()
+				         .orElse(100);
 		//listar los primeros 10 paises con mayor pib del 2020 y mostrar su pib
-		record RegistroPais (String countryName, Double pib) {};
+		record RegistroPais (String countryName, String upper, Double pib) {};
 		var topMayorPib2020 = listInd.stream()
 				               .map(ind -> {
 				            	   var pib = ind.listaYear.stream()
 				            			                      .filter( iy -> iy.year == 2020)
 				            			                      .findFirst();
-				            	   return new RegistroPais(ind.countryName,pib.isPresent() ? pib.get().value : 0.0);
+				            	   return new RegistroPais(ind.countryName, "", pib.isPresent() ? pib.get().value : 0.0);
 				               })
 				               .filter(r -> r.pib > 0)
 				               .sorted( (a,b) -> b.pib.compareTo(a.pib))
-				               .limit(10)
+				               .limit(20)
 				               .toList();
-		System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -");
+		System.out.println("- ".repeat(41));
 		System.out.println("LOS MAS ALTOS 2020");
 		topMayorPib2020.forEach( r -> {
 			String pib = String.format("%.4f", r.pib);
 			pib = " ".repeat(15 - pib.length()) + pib;
-			System.out.println(String.format("%s %s %s",r.countryName, " ".repeat(35 - r.countryName.length()), pib));	
+			System.out.println(String.format("%s %s %s",r.countryName, " ".repeat(maxName - r.countryName.length()), pib));	
 		});
 		//listar los ultimos 10 paises con menor pib del 2019 y mostrar su pib
 		var topMenorPib2019 = listInd.stream()
@@ -85,18 +92,37 @@ public class App {
 	            	   var pib = ind.listaYear.stream()
 	            			                      .filter( iy -> iy.year == 2019)
 	            			                      .findFirst();
-	            	   return new RegistroPais(ind.countryName,pib.isPresent() ? pib.get().value : 0.0);
+	            	   return new RegistroPais(ind.countryName, "", pib.isPresent() ? pib.get().value : 0.0);
 	               })
 	               .filter(r -> r.pib > 0)
 	               .sorted( (a,b) -> a.pib.compareTo(b.pib))
-	               .limit(10)
+	               .limit(20)
 	               .toList();
-		System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -");
+		System.out.println("- ".repeat(41));
 		System.out.println("LOS MAS BAJOS 2019");
 		topMenorPib2019.forEach( r -> {
 			String pib = String.format("%.4f", r.pib);
 			pib = " ".repeat(15 - pib.length()) + pib;
-			System.out.println(String.format("%s %s %s",r.countryName, " ".repeat(35 - r.countryName.length()), pib));	
+			System.out.println(String.format("%s %s %s",r.countryName, " ".repeat(maxName - r.countryName.length()), pib));	
 		});
+		System.out.println("- ".repeat(41));
+		//Promedio del PIB de los ultimos 10 años ordenado por nombre del pais
+		var promedioUlt10Year = listInd.stream()
+				                       .map(ind -> {
+				                    	   var promedio = ind.listaYear.stream()
+				                    			                       .filter(iy -> iy.year >= 2010)
+				                    			                       .mapToDouble( iy -> iy.value)
+				                    			                       .average()
+				                    			                       .orElse(0);
+				                    	   return new RegistroPais(ind.countryName, ind.countryName.toUpperCase(), promedio);
+				                       })
+				                       .sorted( (a,b)-> a.upper.compareTo(b.upper))
+				                       .toList();
+		promedioUlt10Year.forEach( r -> {
+			String pib = String.format("%.4f", r.pib);
+			pib = " ".repeat(15 - pib.length()) + pib;
+			System.out.println(String.format("%s %s %s",r.countryName, " ".repeat(maxName - r.countryName.length()), pib));
+		});
+		
 	}
 }
